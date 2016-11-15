ff72952d2b0612ea0f9143626f624455c4ba4271
Cédric Teyton <cedric.teyton@promyze.com>
2016-11-15 16:02:07 +0100

    low debt

--- src/main/java/ClassFull.java
+++ src/main/java/ClassFull.java
@@ -24,9 +24,6 @@ public class ClassFull {
     public int multiplication2(int a, int b) {
         int c = a * b;
         int k = 0;
-        int l = 0;
-        int m = 0;
-        int n = 0;
         return c;
     }
 
