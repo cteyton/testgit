5fc6bb580f03d3f16bb7fef26983e06e03e02651
Cédric Teyton <cedric.teyton@promyze.com>
2016-11-15 16:05:07 +0100

    test3

--- src/main/java/ClassFull.java
+++ src/main/java/ClassFull.java
@@ -103,5 +103,22 @@ public class ClassFull {
         return c;
     }
 
+    public int multiplication15(int a, int b) {
+        int c = a * b;
+        int k = 0;
+        int l = 0;
+        int m = 0;
+        int n = 0;
+        return c;
+    }
+
+    public int multiplication16(int a, int b) {
+        int c = a * b;
+        int k = 0;
+        int l = 0;
+        int m = 0;
+        int n = 0;
+        return c;
+    }
 
 }
