ffa55aa1542346cef0647a9328af126581cf5c0b
Cédric Teyton <cedric.teyton@promyze.com>
2016-11-24 17:54:22 +0100

    yoooo 2

--- src/main/java/ClassOne.java
+++ src/main/java/ClassOne.java
@@ -26,31 +26,6 @@ public class ClassOne {
         return true;
     }
 
-    public void boo2() {
-        System.out.println("Hello 1");
-        System.out.println("Hello 2");
-        int i = 0;
-        int bar = 1;
-        int bar2 = 1;
-        System.out.println("YOOOO 2");
-    }
-
-    public void boo5() {
-        int i = 0;
-        int bar = 1;
-        int bob = 5;
-        int cat = 7;
-        System.out.println("YOOOO 2");
-    }
-
-    public void boo6() {
-        int i = 0;
-        int bar = 1;
-        int bob = 5;
-        int cat = 7;
-        System.out.println("YOOOO 2");
-    }
-
     public boolean foo12() {
         try {
             System.out.println("Hello");
