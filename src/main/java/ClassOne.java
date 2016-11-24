f5a55f8960bd207f658726d3ac969ce40ee5f70a
Cédric Teyton <cedric.teyton@promyze.com>
2016-11-24 17:49:05 +0100

    yoooo 2

--- src/main/java/ClassOne.java
+++ src/main/java/ClassOne.java
@@ -1,9 +1,3 @@
-import java.awt.color.CMMException;
-import java.awt.color.ICC_ColorSpace;
-import java.awt.dnd.Autoscroll;
-import java.awt.color.ICC_Profile;
-import java.awt.color.ProfileDataException;
-
 /**
  * Created by cteyton on 20/05/16.
  */
@@ -56,26 +50,6 @@ public class ClassOne {
         int cat = 7;
         System.out.println("YOOOO 2");
     }
-
-    public boolean foo11() {
-        try {
-            System.out.println("Hello");
-            System.out.println("Hello 1");
-            System.out.println("Hello 2");
-            System.out.println("Hello 3");
-            System.out.println("Hello 4");
-            System.out.println("Hello 3");
-            System.out.println("Hello 4");
-            System.out.println("Hello 4");
-            System.out.println("Hello 3");
-            System.out.println("Hello 4");
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        return true;
-    }
-
 
     public boolean foo12() {
         try {
