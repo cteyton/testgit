c5295d33be62cb458763ff26afb089f1d170ced3
Cédric Teyton <cedric.teyton@promyze.com>
2016-11-24 17:37:29 +0100

    yoooo 2

--- src/main/java/ClassOne.java
+++ src/main/java/ClassOne.java
@@ -7,24 +7,24 @@ public class ClassOne {
         //I just add a comment
     }
 
-    public boolean foo1() {
-        try {
-            System.out.println("Hello");
-            System.out.println("Hello 1");
-            System.out.println("Hello 2");
-            System.out.println("Hello 3");
-            System.out.println("Hello 4");
-            System.out.println("Hello 3");
-            System.out.println("Hello 4");
-            System.out.println("Hello 4");
-            System.out.println("Hello 3");
-            System.out.println("Hello 4");
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        return true;
-    }
+//    public boolean foo1() {
+//        try {
+//            System.out.println("Hello");
+//            System.out.println("Hello 1");
+//            System.out.println("Hello 2");
+//            System.out.println("Hello 3");
+//            System.out.println("Hello 4");
+//            System.out.println("Hello 3");
+//            System.out.println("Hello 4");
+//            System.out.println("Hello 4");
+//            System.out.println("Hello 3");
+//            System.out.println("Hello 4");
+//        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        return true;
+//    }
 
     public void boo2() {
         System.out.println("Hello 1");
@@ -32,25 +32,6 @@ public class ClassOne {
         int i = 0;
         int bar = 1;
         int bar2 = 1;
-        System.out.println("YOOOO 2");
-    }
-
-    public void boo3() {
-        int i = 0;
-        System.out.println("YOOOO 2");
-        System.out.println("Hello 4");
-        int bar = 1;
-        int lol = 3;
-        int yoyo = 5;
-        int i2 = 0;
-        int bar2 = 1;
-    }
-
-    public void boo4() {
-        int i = 0;
-        int bar = 1;
-        int bob = 5;
-        int cat = 7;
         System.out.println("YOOOO 2");
     }
 
