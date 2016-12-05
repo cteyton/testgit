36d98fa9a5d95e1b8f76b077ec3cb0e3120b1853
Cédric Teyton <cedric.teyton@promyze.com>
2016-12-05 17:16:47 +0100

    yo

--- src/main/java/ClassOne.java
+++ src/main/java/ClassOne.java
@@ -32,6 +32,11 @@ public class ClassOne {
             System.out.println("Hello 3");
             System.out.println("Hello 4");
             System.out.println("Hello 3");
+
+            System.out.println("Hello 1");
+            System.out.println("Hello 2");
+            System.out.println("Hello 3");
+            System.out.println("Hello 4");
         }
         catch (Exception e){
             e.printStackTrace();
