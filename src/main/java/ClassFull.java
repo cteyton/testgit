91b7b17c07c9e19e09fc82e109cc1f85e60bf334
Cédric Teyton <cedric.teyton@promyze.com>
2016-12-13 16:13:09 +0100

    fixes

--- src/main/java/ClassFull.java
+++ src/main/java/ClassFull.java
@@ -6,6 +6,10 @@ public class ClassFull {
 
     public static void main(String[] args) {
         System.out.println("Hello world !");
+        System.out.println("Hello world 1 !");
+        System.out.println("Hello world 2 !");
+        System.out.println("Hello world 3 !");
+        System.out.println("Hello world 4 !");
     }
 
     public int addition(int a, int b) {
