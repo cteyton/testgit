83cfb7e05324a67bd23b3b1e4acc93f9f300c99e
Cédric Teyton <cedric.teyton@promyze.com>
2016-12-15 15:55:24 +0100

    fixes

---
+++ src/main/java/NewWithDebt.java
@@ -0,0 +1,21 @@
+/**
+ * Created by cteyton on 15/12/16.
+ */
+public class NewWithDebt {
+
+    public boolean foo4() {
+        try {
+            System.out.println("Hello");
+            System.out.println("Hello 4");
+            System.out.println("Hello 3");
+            System.out.println("Hello");
+            System.out.println("Hello 1");
+            System.out.println("Hello 4");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+}
