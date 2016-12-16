14de69109ffd3a98295eac777ac1133765b1d48d
Cédric Teyton <cedric.teyton@promyze.com>
2016-12-16 09:51:37 +0100

    DET : Add more unit tests
    TFI : Fix in itemsToDos
    
    COV : Fix in itemsToDos

--- src/main/java/ClassOne.java
+++ src/main/java/ClassOne.java
@@ -16,9 +16,6 @@ public class ClassOne {
             System.out.println("Hello 2");
             System.out.println("Hello 3");
             System.out.println("Hello 4");
-            System.out.println("Hello 3");
-            System.out.println("Hello");
-            System.out.println("Hello 1");
             System.out.println("Hello 4");
             System.out.println("Hello 3");
             System.out.println("Hello 4");
