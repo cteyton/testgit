16701db61ae61e1270ba127cdc4bc8c750648291
Cédric Teyton <cedric.teyton@promyze.com>
2016-12-16 09:15:09 +0100

    fixes

--- src/main/java/ClassOne.java
+++ src/main/java/ClassOne.java
@@ -113,8 +113,27 @@ public class ClassOne {
         }
         try {
             System.out.println("Hello");
+            System.out.println("Hello 1");
+            System.out.println("Hello 4");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        i = new Random().nextInt(40);
+        if(i > 10) {
             System.out.println("Hello 4");
             System.out.println("Hello 3");
+        }
+        return true;
+    }
+
+    public boolean foo9() {
+        int i = new Random().nextInt(40);
+        if(i > 10) {
+            System.out.println("Hello 4");
+            System.out.println("Hello 3");
+        }
+        try {
             System.out.println("Hello");
             System.out.println("Hello 1");
             System.out.println("Hello 4");
@@ -129,5 +148,28 @@ public class ClassOne {
         }
         return true;
     }
+
+    public boolean foo10() {
+        int i = new Random().nextInt(40);
+        if(i > 10) {
+            System.out.println("Hello 4");
+            System.out.println("Hello 3");
+        }
+        try {
+            System.out.println("Hello");
+            System.out.println("Hello 1");
+            System.out.println("Hello 4");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        i = new Random().nextInt(40);
+        if(i > 10) {
+            System.out.println("Hello 4");
+            System.out.println("Hello 3");
+        }
+        return true;
+    }
+
 
 }
