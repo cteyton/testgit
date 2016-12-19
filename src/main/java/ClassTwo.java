53405db1a60b3f4811b5afb55412a1d3a250418f
Cédric Teyton <cedric.teyton@promyze.com>
2016-12-19 07:47:39 +0100

    fixes

--- src/main/java/ClassTwo.java
+++ src/main/java/ClassTwo.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Created by cteyton on 20/05/16.
  */
@@ -33,5 +35,43 @@ public class ClassTwo {
         }
         return true;
     }
+
+    public boolean foo7() {
+        try {
+            System.out.println("Hello 3");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        int i = new Random().nextInt(40);
+        if(i > 10) {
+            System.out.println("Hello 3");
+        }
+        return true;
+    }
+
+    public boolean foo8() {
+        int i = new Random().nextInt(40);
+        if(i > 10) {
+            System.out.println("Hello 4");
+            System.out.println("Hello 3");
+        }
+        try {
+            System.out.println("Hello");
+            System.out.println("Hello 1");
+            System.out.println("Hello 4");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        i = new Random().nextInt(40);
+        if(i > 10) {
+            System.out.println("Hello 4");
+            System.out.println("Hello 3");
+        }
+        return true;
+    }
+
+
 
 }
