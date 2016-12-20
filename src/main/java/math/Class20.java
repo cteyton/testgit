1044cc200d95bcc73093eb5ddb3387f9303743ce
Cédric Teyton <cedric.teyton@promyze.com>
2016-12-20 11:45:32 +0100

    fixes

---
+++ src/main/java/math/Class20.java
@@ -0,0 +1,16 @@
+package math;
+
+/**
+ * Created by cteyton on 20/12/16.
+ */
+public class Class20 {
+
+    public int addition(int a, int b) {
+        System.out.println("Hello world 2 !");
+        System.out.println("Hello world 4 !");
+        int c = a + b;
+        //Hello world
+        return c;
+    }
+
+}
