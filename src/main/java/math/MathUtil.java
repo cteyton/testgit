4363c5ea88740ee9bb3247d0220569876351cdcc
Cédric Teyton <cedric.teyton@promyze.com>
2016-12-20 10:57:56 +0100

    fixes

---
+++ src/main/java/math/MathUtil.java
@@ -0,0 +1,33 @@
+package math;
+
+/**
+ * This class should only have 10% of coverage
+ */
+public class MathUtil {
+
+    public void foo1(){
+        int i = 0;
+    }
+
+    public void foo2(){
+        int i = 0;
+    }
+
+    public int multiplication21(int a, int b) {
+        int c = a * b;
+        int k = 0;
+        return c;
+    }
+
+    public int multiplication30(int a, int b) {
+        int c = a * b;
+        int k = 0;
+        return c;
+    }
+
+    public int multiplication31(int a, int b) {
+        int c = a * b;
+        return c;
+    }
+
+}
