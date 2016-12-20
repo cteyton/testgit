de6a0b8ae138e5373a0d2fcc406697ddd9e5ada2
Cédric Teyton <cedric.teyton@promyze.com>
2016-12-20 10:46:18 +0100

    fixes

---
+++ src/main/java/math/Addition.java
@@ -0,0 +1,18 @@
+package math;
+
+/**
+ * Created by cteyton on 20/12/16.
+ */
+public class Addition {
+
+    
+    public int addition(int a, int b) {
+        System.out.println("Hello world 2 !");
+        System.out.println("Hello world 4 !");
+        int c = a + b;
+        //Hello world
+        return c;
+    }
+
+
+}
