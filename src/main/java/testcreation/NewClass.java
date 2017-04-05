a1e1e31118d3f3554a898afed5d724966a0e07e2
Cédric Teyton <cedric.teyton@promyze.com>
2017-04-05 08:00:03 +0200

    create a new beautiful class

---
+++ src/main/java/testcreation/NewClass.java
@@ -0,0 +1,21 @@
+package testcreation;
+
+/**
+ * Created by cteyton on 05/04/17.
+ */
+public class NewClass {
+
+    private int cpt;
+
+    public NewClass(){
+        //This is a constructor
+    }
+
+    public int getCpt() {
+        return cpt;
+    }
+
+    public void setCpt(int cpt) {
+        this.cpt = cpt;
+    }
+}
