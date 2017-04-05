547e2badf9a0d5935c0101ce292712d2553ee299
Cédric Teyton <cedric.teyton@promyze.com>
2017-04-05 08:45:18 +0200

    create a new beautiful class 6

---
+++ src/main/java/testcreation/NewClass6.java
@@ -0,0 +1,21 @@
+package testcreation;
+
+/**
+ * Created by cteyton on 05/04/17.
+ */
+public class NewClass6 {
+
+    private int cpt;
+
+    public NewClass6(){
+        //This is a constructor
+    }
+
+    public int getCpt() {
+        return cpt;
+    }
+
+    public void setCpt(int cpt) {
+        this.cpt = cpt;
+    }
+}
