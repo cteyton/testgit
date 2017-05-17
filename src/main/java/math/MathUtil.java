647d297091be1152efbac6cc0c863be3aa7d5b45
Cédric Teyton <cedric.teyton@promyze.com>
2017-05-17 08:48:57 +0200

    math util

--- src/main/java/math/MathUtil.java
+++ src/main/java/math/MathUtil.java
@@ -5,29 +5,53 @@ package math;
  */
 public class MathUtil {
 
+    /**
+     * Multiplication
+     */
     public void foo1(){
-        int i = 0;
+        int myIValue = 0;
     }
 
+    /**
+     * Multiplication
+     */
     public void foo2(){
-        int i = 0;
+        int myIValue = 0;
     }
 
+    /**
+     * Multiplication
+     * @param op1 baba
+     * @param op2 bob
+     * @return
+     */
     public int multiplication21(int op1, int op2) {
-        int cValue = op1 * op2;
-        int kValue = 0;
-        return cValue;
+        int myCValue = op1 * op2;
+        int myKValue = 0;
+        return myCValue;
     }
 
+    /**
+     * Multiplication
+     * @param op1 baba
+     * @param op2 bob
+     * @return
+     */
     public int multiplication30(int op1, int op2) {
-        int cValue = op1 * op2;
-        int kValue = 0;
-        return cValue;
+        int myCValue = op1 * op2;
+        int myKValue = 0;
+        return myCValue;
     }
 
+    /**
+     * Multiplication
+     * @param op1 baba
+     * @param op2 bob
+     * @return
+     */
     public int multiplication31(int op1, int op2) {
-        int cValue = op1 * op2;
-        return cValue;
+        int myCValue = op1 * op2;
+        return myCValue;
     }
 
 }
