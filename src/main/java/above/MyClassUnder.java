63abae1859107f6895c0591ae6b6b52977046d49
Cédric Teyton <cedric.teyton@promyze.com>
2017-05-18 07:57:07 +0200

    Degradation de MyClassUnder

--- src/main/java/above/MyClassUnder.java
+++ src/main/java/above/MyClassUnder.java
@@ -1,5 +1,14 @@
 package above;
 
+import java.util.*;
+import java.lang.*;
+import java.awt.*;
+import java.applet.*;
+import java.beans.*;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+
 /**
  * Created by cteyton on 09/09/16.
  */
@@ -34,6 +43,34 @@ public class MyClassUnder {
     }
 
     public int add13(int a, int b) {
+        return a+b;
+    }
+
+    public int add14(int a, int b) {
+        return a+b;
+    }
+
+    public int add15(int a, int b) {
+        return a+b;
+    }
+
+    public int add16(int a, int b) {
+        return a+b;
+    }
+
+    public int add17(int a, int b) {
+        return a+b;
+    }
+
+    public int add18(int a, int b) {
+        return a+b;
+    }
+
+    public int add19(int a, int b) {
+        return a+b;
+    }
+
+    public int add20(int a, int b) {
         return a+b;
     }
 
