f6c1935725afdadf11c5ef3ffabb208f2b0c3f2d
Cédric Teyton <cedric.teyton@promyze.com>
2017-05-18 08:08:25 +0200

    Clean my ClassUnder

--- src/main/java/above/MyClassUnder.java
+++ src/main/java/above/MyClassUnder.java
@@ -1,95 +1,28 @@
 package above;
 
-import java.util.*;
 import java.lang.*;
-import java.awt.*;
-import java.applet.*;
-import java.beans.*;
-import java.io.*;
-import java.math.*;
-import java.security.*;
 
 /**
  * Created by cteyton on 09/09/16.
  */
 public class MyClassUnder {
 
-    public int add0(int a, int b){
-        return a+b;
-    }
-
-    public int add1(int a, int b){
-        return a+b;
-    }
-
-    public int add2(int a, int b){
-        return a+b;
-    }
-
-    public int add3(int a, int b){
-        return a+b;
-    }
-
-    public int add6(int a, int b){
-        return a+b;
-    }
-
-    public int add7(int a, int b){
-        return a+b;
-    }
-
-    public int add12(int a, int b) {
-        return a+b;
-    }
-
-    public int add13(int a, int b) {
-        return a+b;
-    }
-
-    public int add14(int a, int b) {
-        return a+b;
-    }
-
-    public int add15(int a, int b) {
-        return a+b;
-    }
-
-    public int add16(int a, int b) {
-        return a+b;
-    }
-
-    public int add17(int a, int b) {
-        return a+b;
-    }
-
-    public int add18(int a, int b) {
-        return a+b;
-    }
-
-    public int add19(int a, int b) {
-        return a+b;
-    }
-
-    public int add20(int a, int b) {
-        return a+b;
-    }
-
     /**
      * Sane
-     * @param a
-     * @param b
-     * @return
+     * @param op1
+     * @param op2
+     * @return a great value
      */
-    public int multiplication30(int a, int b) {
-        int c = a * b;
-        int k = 0;
+    public int multiplication(int op1, int op2) {
+        int myCValue = op1 * op2;
+        int myKValue = 0;
         try {
             System.out.println("Hello 4");
         }
         catch (Exception e){
             e.printStackTrace();
         }
-        return c;
+        return myCValue;
     }
 
 }
